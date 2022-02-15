789ce2fc4f336eafa05dea573d51117a4c451cf9
jszafran <jszafran.pv@gmail.com>
2022-02-15 19:08:01 +0100

    Bring back setup.py and add Faker to depedencies.

--- setup.py
+++ setup.py
@@ -20,4 +20,7 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=[
+        "faker",
+    ]
 )
